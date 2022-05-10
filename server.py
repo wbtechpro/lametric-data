d968cc2b8a993589a58eb01031291cdc56359e9b
robert <saakyan-robert@rambler.ru>
2022-05-10 21:46:47 +0300

    разные эндпоинты

--- server.py
+++ server.py
@@ -1,9 +1,21 @@
 from puller import FramesCatalog
 
 
+def test():
+    return '111'
+
+
+URLS = {'/finolog': FramesCatalog().get_frames_json}
+
+
 def application(environ, start_response):
-    status = '200 OK'
     headers = [('Content-type', 'application/json; charset=utf-8')]
-    start_response(status, headers)
-    response = FramesCatalog().get_frames_json()
-    return [response.encode()]
+    try:
+        current_handler = URLS[environ['PATH_INFO']]
+        status = '200 OK'
+        start_response(status, headers)
+    except KeyError:
+        status = '404 Not Found'
+        start_response(status, headers)
+        return
+    return [current_handler().encode()]
