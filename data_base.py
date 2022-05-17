ba78a48a9daf0831a7b4061bd559543a7650eb6c
robert <saakyan-robert@rambler.ru>
2022-05-17 17:35:19 +0300

    БД, новые экраны

---
+++ data_base.py
@@ -0,0 +1,58 @@
+import sqlite3
+
+
+def data_base(environ, insert=None, ):
+    conn = sqlite3.connect('sqlite.db')
+    cur = conn.cursor()
+    cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    all_tables = cur.fetchall()
+
+    def del_table(table):
+        cur.execute("DROP TABLE {};".format(table))
+
+    def create_insert_table(table_name):
+        for table in all_tables:
+            if table != ('sqlite_sequence',) and table_name in table[0]:
+                del_table(*table)
+        cur.execute(f"CREATE TABLE IF NOT EXISTS '{table_name}' ("
+                    "'name' VARCHAR(50),"
+                    "'value' INTEGER) ")
+        for key, value in insert.items():
+            if isinstance(value, int):
+                cur.execute(f"INSERT INTO {table_name} VALUES (?, ?)", (key, value,))
+                conn.commit()
+            elif isinstance(value, dict):
+                cur.execute(f"CREATE TABLE IF NOT EXISTS '{key}_{table_name}' ("
+                            "'name' VARCHAR(50),"
+                            "'value' INTEGER) ")
+                cur.execute(f'DELETE FROM "{key}_{table_name}"')
+                for k, obj in value.items():
+                    cur.execute(f"INSERT INTO '{key}_{table_name}' VALUES (?, ?)", (k, obj))
+                    conn.commit()
+
+    def get_data(check_table):
+        all_result = dict()
+        all_result[check_table] = []
+        for table in all_tables:
+            if table != ('sqlite_sequence',):
+                if check_table in table[0]:
+                    cur.execute("SELECT  * FROM {};".format(*table))
+                    one_result = cur.fetchall()
+                    if table[0] == check_table:
+                        all_result[check_table].append(one_result)
+                    else:
+                        all_result[check_table].append(
+                            {'{}'.format(table[0].split('_')[0]): one_result})
+        return all_result
+
+    if insert is not None and environ['REQUEST_METHOD'] == 'POST':
+        if environ['PATH_INFO'] == '/webhooks/marketing':
+            create_insert_table('MARKETING')
+        elif environ['PATH_INFO'] == '/webhooks/recruiting':
+            create_insert_table('RECRUITING')
+    else:
+        if environ['PATH_INFO'] == '/marketing':
+            return get_data('MARKETING')['MARKETING']
+        elif environ['PATH_INFO'] == '/recruiting':
+            return get_data('RECRUITING')['RECRUITING']
+    conn.close()
--- server.py
+++ server.py
@@ -1,17 +1,39 @@
+import json
+
 from puller import FramesCatalog
+from marketing import marketing_recruiting
+from data_base import data_base
 
+URLS = {'/finolog': FramesCatalog().get_frames_json,
+        '/marketing': marketing_recruiting,
+        '/recruiting': marketing_recruiting}
 
-URLS = {'/finolog': FramesCatalog().get_frames_json}
+POST_URLS = ['/webhooks/marketing', '/webhooks/recruiting']
 
 
 def application(environ, start_response):
     headers = [('Content-type', 'application/json; charset=utf-8')]
     try:
-        current_handler = URLS[environ['PATH_INFO']]
-        status = '200 OK'
-        start_response(status, headers)
-    except KeyError:
+        if not environ['REQUEST_METHOD'] == 'POST':
+            current_handler = URLS[environ['PATH_INFO']]
+            status = '200 OK'
+            start_response(status, headers)
+            response = [current_handler(data_base(environ)).encode()]
+            if environ['PATH_INFO'] == '/finolog':
+                response = [current_handler().encode()]
+        elif environ['PATH_INFO'] in POST_URLS:
+            status = '200 OK'
+            start_response(status, headers)
+            request_body = environ['wsgi.input'].read(int(environ.get('CONTENT_LENGTH', 0)))
+            request_body_json = json.loads(request_body)
+            data_base(environ, request_body_json)
+            response = [json.dumps(request_body_json).encode()]
+        else:
+            status = '404 Not Found'
+            start_response(status, headers)
+            response = [json.dumps(dict(detail=status)).encode()]
+    except:
         status = '404 Not Found'
         start_response(status, headers)
-        return
-    return [current_handler().encode()]
+        response = [json.dumps(dict(detail=status)).encode()]
+    return response
